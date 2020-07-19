3b9f0ed46b213b3feb52734bba64cb8458bb397b
Царюк Артём <42806772+funcid@users.noreply.github.com>
2020-07-19 17:33:23 +0300

    Интеграция VK

--- src/main/java/mangal10n/service/App.java
+++
@@ -1,8 +0,0 @@
-package mangal10n.service;
-
-/**
- * @author func 19.07.2020
- * @project mangal10n
- */
-public class App {
-}
---
+++ src/main/java/mangal10n/vk/App.java
@@ -0,0 +1,33 @@
+package mangal10n.vk;
+
+import com.petersamokhin.vksdk.core.client.VkApiClient;
+import com.petersamokhin.vksdk.core.http.HttpClient;
+import com.petersamokhin.vksdk.core.model.VkSettings;
+import com.petersamokhin.vksdk.core.model.objects.Message;
+import com.petersamokhin.vksdk.http.VkOkHttpClient;
+
+/**
+ * @author func 19.07.2020
+ * @project mangal10n
+ */
+public class App {
+
+	public static void main(String[] args) {
+		int groupId = 197254109;
+		String accessToken = "ACCESS";
+		HttpClient vkHttpClient = new VkOkHttpClient();
+
+		VkApiClient client = new VkApiClient(groupId, accessToken, VkApiClient.Type.Community, new VkSettings(vkHttpClient));
+
+		client.onMessage(event -> {
+			System.out.println(event.getMessage().toString());
+			new Message()
+					.peerId(event.getMessage().getPeerId())
+					.text("Hello, world!")
+					.sendFrom(client)
+					.execute();
+		});
+
+		client.startLongPolling();
+	}
+}
